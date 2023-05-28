d70114656792a0c035f9bd936e2003d032eb0573
Linfos5 <93025848+Linfos5@users.noreply.github.com>
2023-05-29 00:38:50 +0400

    Update Task 3.py

--- lesson_2/Task 3.py
+++ lesson_2/Task 3.py
@@ -1,3 +1,12 @@
-a = int(input("Введите стоимость товара: "))
-b = a // 100 * 50
-print("Цена товара с учётом скидки - ", a - b)
+price = float(input("Введите стоимость товара: "))
+if price > 1500:
+    discount = 20
+elif price > 1000:
+    discount = 15
+elif price > 500:
+    discount = 10
+elif price > 100:
+    discount = 5
+amount = price * discount / 100
+price_with_discount = price - amount
+print(f"Цена товара со скидкой", discount, "%:", price_with_discount)
