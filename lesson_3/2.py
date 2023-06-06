941d81c476ba237d14036737d0cc28b8d82577b1
Linfos5 <93025848+Linfos5@users.noreply.github.com>
2023-06-06 22:51:56 +0400

    Update 2.py

--- lesson_3/2.py
+++ lesson_3/2.py
@@ -1,49 +1,37 @@
 HttpCode = int(input("Введите http-код: "))
 
-code1xx = HttpCode
-code2xx = HttpCode
-code3xx = HttpCode
-code4xx = HttpCode
-code5xx = HttpCode
-
-match code1xx:
+match HttpCode:
     case 100:
         print("Continue")
     case 102:
         print("Processing")
     case "103":
-        print("Early Hints")    
-    case _:
+        print("Early Hints")
+    case code1xx if HttpCode < 200:
         print("Неизвестный код для группы 1xx")
-            
-match code2xx:
     case 200:
         print("OK")
     case 204:
         print("No Content")
-    case _:
+    case code2xx if HttpCode < 300:
         print("Неизвестный код для группы 2xx")
-
-match code3xx:
     case 302:
         print("Found")
     case 305:
         print("Use Proxy")
-    case _:
+    case code3xx if HttpCode < 400:
         print("Неизвестный код для группы 3xx")
-            
-match code4xx:
     case 404:
         print("Not Found")
     case 409:
         print("Conflict")
-    case _:
+    case code4xx if HttpCode < 500:
         print("Неизвестный код для группы 4xx")
-            
-match code5xx:
     case 503:
         print("Service Unavailable")
     case 510:
         print("Not Extended")
-    case _:
+    case code5xx if HttpCode < 600:
         print("Неизвестный код для группы 5xx")
+    case _:
+        print("Неизвестный код")
