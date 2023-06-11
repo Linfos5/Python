cbfc2fb99015a0478aae40a44ac4cd9bb0e3a67f
Linfos5 <93025848+Linfos5@users.noreply.github.com>
2023-06-11 18:52:57 +0400

    Create 2.py

---
+++ lesson_4/2.py
@@ -0,0 +1,15 @@
+import random
+
+N = random.randint(1, 100)
+user = 0
+
+while user != N:
+    user = int(input("Угадай число от 1 до 100: "))
+    if user > N:
+        print("Число должно быть меньше")
+    elif user < N:
+        print("Число должно быть больше")
+    else:
+        print("Вы угадали число!")
+
+    
