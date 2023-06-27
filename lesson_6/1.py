07dcd92497a146ac2771701c655d56e8cce2dd0b
Linfos5 <93025848+Linfos5@users.noreply.github.com>
2023-06-27 12:04:50 +0400

    Create 1.py

---
+++ lesson_6/1.py
@@ -0,0 +1,13 @@
+print(1)
+a = "Hello world\n"
+print(a*10)
+
+print(2)
+for i in range(10):
+    print("Hello world")
+
+print(3)
+i = 0
+while i < 10:
+    print("Hello, world", end ' ')
+    i += 1
