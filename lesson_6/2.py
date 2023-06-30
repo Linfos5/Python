c930e22f677141c31613535e51861ab52d14c4b6
Linfos5 <93025848+Linfos5@users.noreply.github.com>
2023-06-30 17:58:40 +0400

    Update 2.py

--- lesson_6/2.py
+++ lesson_6/2.py
@@ -1,6 +1,6 @@
 num = input("Введите числа через пробел: ")
 lst = num.split()
-lst = [float(n) for n in lst]
+lst = [int(n) for n in lst]
 
 odd = [n for n in lst if n % 2 != 0]
 even = [n for n in lst if n % 2 == 0]
